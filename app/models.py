f3d3c4867c387aca0b680090c689f3a68ce7814a
liazylee <li233111@163.com>
2017-09-30 13:43:35 +0800

    V1.5

--- app/models.py
+++ app/models.py
@@ -69,6 +69,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
     name = db.Column(db.String(64))
+    Wechat=db.Column(db.String(64))
     location = db.Column(db.String(64))
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
